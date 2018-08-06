44bc42e57a2edd8c12f4596e4f049da998039ba5
GlennCavarle <glenn.cavarle@libre-informatique.fr>
2018-08-06 16:40:02 +0200

    add service/index.ts and update services and models aliasing

--- src/api/models.ts
+++ src/api/models.ts
@@ -1,14 +1,17 @@
 //temporary model aliasing
 //to avoid naming collision with old PIA-CNIL models
 
-export { Processing as ProcessingModel } from '@api/model';
-export { Pia as PiaModel } from '@api/model';
-export { Answer as AnswerModel } from '@api/model';
-export { Comment as CommentModel } from '@api/model';
-export { Evaluation as EvaluationModel } from '@api/model';
-export { Measure as MeasureModel } from '@api/model';
-export { Attachment as AttachmentModel } from '@api/model';
-export { UserProfile as UserProfileModel } from '@api/model';
-export { UserToken as UserTokenModel } from '@api/model';
-export { Template as TemplateModel } from '@api/model';
-export { Folder as FolderModel } from '@api/model';
+export {
+    Processing as ProcessingModel,
+    Pia as PiaModel,
+    Answer as AnswerModel,
+    Comment as CommentModel,
+    Evaluation as EvaluationModel,
+    Measure as MeasureModel,
+    Attachment as AttachmentModel,
+    UserProfile as UserProfileModel,
+    UserToken as UserTokenModel,
+    Template as TemplateModel,
+    Folder as FolderModel
+
+} from '@api/model';
