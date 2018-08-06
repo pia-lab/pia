a87fee56b42fc31df15cea9a01b815b8c2d06485
Romain Sanchez <romainsancho@gmail.com>
2018-08-06 19:32:38 +0200

    Add TinyMCE to processing form fields

--- src/app/processing/processing-form/processing-form.component.ts
+++ src/app/processing/processing-form/processing-form.component.ts
@@ -1,7 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { FormGroup, FormControl, NgForm } from '@angular/forms';
 
 import { ProcessingModel } from '@api/models';
 import { ProcessingApi } from '@api/services';
+
 
 @Component({
   selector: 'app-processing-form',
@@ -9,18 +11,28 @@ import { ProcessingApi } from '@api/services';
   styleUrls: ['./processing-form.component.scss']
 })
 
-export class ProcessingFormComponent {
+export class ProcessingFormComponent implements OnInit, OnDestroy {
   @Input() sections: any;
   @Input() processing: ProcessingModel;
   @Input() currentSection: any;
+  @ViewChild('processingForm') processingForm: NgForm;
+  editor: any;
+  elementId: String;
 
   constructor(
     public processingApi: ProcessingApi,
+    private ref: ChangeDetectorRef
   ) { }
 
+  ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    this.closeEditor();
+  }
+
   updateProcessing() {
-    console.log(this.processing);
-    this.processingApi.update(this.processing);
+    this.processingApi.get(this.processing.id);
   }
 
   getSectionById(sectionId) {
@@ -29,5 +41,61 @@ export class ProcessingFormComponent {
 
   updateKnowledgeBase(item: any) {
 
+  }
+
+  editField(element: any) {
+    // this.permissionsService.hasPermission('CanEditPIA').then((hasPerm: boolean) => {
+      //     if (hasPerm && this._globalEvaluationService.answerEditionEnabled) {
+        this.elementId = element.id;
+
+        this.loadEditor(element);
+        this.updateKnowledgeBase(element);
+      //     }
+      //   });
+  }
+
+  /**
+   * Load wysiwyg editor.
+   * @memberof ProcessingFormComponent
+   */
+  loadEditor(element: any) {
+    tinymce.init({
+      branding: false,
+      menubar: false,
+      statusbar: false,
+      plugins: 'autoresize lists',
+      forced_root_block: false,
+      autoresize_bottom_margin: 30,
+      auto_focus: element.id,
+      autoresize_min_height: 40,
+      content_style: 'body {background-color:#eee!important;}',
+      selector: '#' + element.id,
+      toolbar: 'undo redo bold italic alignleft aligncenter alignright bullist numlist outdent indent',
+      skin_url: 'assets/skins/lightgray',
+      setup: editor => {
+        this.editor = editor;
+
+        editor.on('focusout', () => {
+          const content = editor.getContent();
+
+          this.processingForm.form.controls[element.name].patchValue(content);
+
+          this.closeEditor();
+          this.updateProcessing();
+          // Hack to trigger view update
+          this.ref.detectChanges();
+        });
+      },
+    });
+  }
+
+  /**
+   * Close wysiwig editor.
+   * @private
+   * @memberof QuestionsComponent
+   */
+  private closeEditor() {
+    tinymce.remove(this.editor);
+    this.editor = null;
   }
 }
