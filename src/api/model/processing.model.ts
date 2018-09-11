b67870875ec21be860ba2aa74a22c5a09dbb8943
PapsOu <vivien.frasca@gmail.com>
2018-09-11 16:51:34 +0200

    Add ask for validation button

--- src/api/model/processing.model.ts
+++ src/api/model/processing.model.ts
@@ -1,11 +1,11 @@
 
 import { BaseModel } from '@api/model/base.model';
-import { PiaModel, FolderModel } from '@api/models';
+import { FolderModel } from '@api/models';
 
 export class Processing extends BaseModel {
   public id: any;
   public name: string;
-  public status: string;
+  public status: number;
   public description: string;
   public author: string;
   public life_cycle: string;
@@ -18,4 +18,11 @@ export class Processing extends BaseModel {
   public processing_data_types: any;
   public folder: FolderModel;
   public pias_count: number;
+}
+
+export enum ProcessingStatus {
+  STATUS_DOING = 0,
+  STATUS_UNDER_VALIDATION = 1,
+  STATUS_VALIDATED = 2,
+  STATUS_ARCHIVED = 3
 }
